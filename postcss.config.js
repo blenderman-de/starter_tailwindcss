c3694594d7b5023925c359d759fabadb9c782493
Oliver Nürnberger <o.nuernberger@blue-silver.com>
2020-05-27 19:02:20 +0200

    init

---
+++ postcss.config.js
@@ -0,0 +1,30 @@
+const tailwindcss = require('tailwindcss');
+
+const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
+
+const plugins = [];
+plugins.push(tailwindcss)
+// plugins.push(tailwindcss('tailwind.config.js'))
+// This is if you want to include your custom config
+
+if (!IS_DEVELOPMENT) {
+    const purgecss = require('@fullhuman/postcss-purgecss');
+    
+    const TailwindExtractor = content => {
+        return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+      };
+    
+    plugins.push(
+        purgecss({
+            content: ['src/*.html'],
+            extractors: [
+                {
+                    extractor: TailwindExtractor,
+                    extensions: ['html']
+                }
+            ],
+        })
+    );
+}
+
+module.exports = { plugins }
---
+++ readme.md
@@ -0,0 +1,35 @@
+Ready-to-use starter project for TailwindCSS and [Parcel](https://parceljs.org/) bundler.  
+
+
+## Install 
+
+Install dependencies from `package.json`.
+
+    npm install 
+
+## Usage
+
+run dev server or build your project to `dist`
+
+    npm run dev 
+    npm run build 
+
+
+change repro version in `package.json` and commit version tag 
+
+    npm version major
+    npm version minor
+    npm version patch 
+
+ 
+Windows CMD only: Create named release in `release` folder. 
+
+    npm run release 
+
+## Awesome TailwindCSS 
+
+- https://mertjf.github.io/tailblocks/ 
+- https://medium.com/@adisk/how-to-setup-tailwind-css-with-parcel-bundler-f76e4aac5f16
+- https://github.com/aniftyco/awesome-tailwindcss
+
+
